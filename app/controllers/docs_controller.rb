c634aec4759f53f12193bfb63bb813a92f215d43
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-02-28 23:09:43 -0600

    Association between User and Files

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,4 +1,6 @@
 class DocsController < ApplicationController
+
+  # - DRY, finds the doc necessary for these actions so that we do not repeat ourselves
   before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -9,14 +11,12 @@ class DocsController < ApplicationController
   end
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs.build
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
 
-    # @doc = current_user.docs.build(doc_params)
-    #
     if @doc.save
       redirect_to @doc
     else
